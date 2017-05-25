f2c28525e948ba2126270456d3f7ec25a5604994
asd <asd>
2017-05-25 19:02:25 +0100

    d
    adsada

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $(".butao").click(function(event) {
     console.log(document.getElementById('botao').value);
-    document.getElementById('texto').innerHTML += "<h1>" + document.getElementById('inputerino').value + "</h1>";
+    document.getElementById('texto').innerText +=  document.getElementById('inputerino').value;
+    document.getElementById('texto').innerHTML +=  "</br>";
 });
